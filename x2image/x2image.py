de3bb077fc72517601790f9085d84751f1115620
Sooty <7614538+SootyOwl@users.noreply.github.com>
2024-07-13 15:08:56 +0100

    refactor: Update x2image.py to use --hide-scrollbars, --disable-gpu, and --no-sandbox flags for Html2Image initialization

--- x2image/x2image.py
+++ x2image/x2image.py
@@ -20,7 +20,7 @@ class X2Image(commands.Cog):
         self.hti = Html2Image(
             output_path=f"{path}/images/",  # where to save the images
             temp_path=f"{path}/tmp/",  # where to save temporary files
-            custom_flags=["--virtual-time-budget=5000", "--hide-scrollbars", "--disable-software-rasterizer"],
+            custom_flags=["--virtual-time-budget=5000", "--hide-scrollbars", "--disable-gpu", "--no-sandbox"],
         )
 
     @commands.hybrid_command(name="x2image")
