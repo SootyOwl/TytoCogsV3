07ec8e85cc30147a7f88eef6d6e4147b44afdf7e
Sooty <7614538+SootyOwl@users.noreply.github.com>
2025-04-28 20:48:44 +0000

    feat(ispyfj): use a session with cookies for bypassing login required links

--- ispyfj/ispyfj.py
+++ ispyfj/ispyfj.py
@@ -3,7 +3,7 @@ from io import BytesIO
 import requests
 from bs4 import BeautifulSoup
 from discord import File
-from redbot.core import Config, app_commands, commands
+from redbot.core import Config, commands
 from redbot.core.bot import Red
 
 
@@ -14,6 +14,45 @@ class IspyFJ(commands.Cog):
         super().__init__(*args, **kwargs)
         self.bot = bot
 
+        # the two cookies needed to bypass login protection
+        self.default_global = {
+            "userId": "1802371",  # can be anything
+            "fjsession": None,
+        }
+        self.config = Config.get_conf(cog_instance=self, identifier=2985734269283475620983465, force_registration=True)
+        self.config.register_global(**self.default_global)
+        self.session = requests.Session()
+
+    async def cog_load(self) -> None:
+        """Load the cog."""
+        self.session.cookies.set("fjsession", await self.config.userId())
+        self.session.cookies.set("userId", await self.config.fjsession())
+
+    async def cog_unload(self) -> None:
+        """Unload the cog."""
+        self.session.cookies.clear()
+
+    @commands.group(name="fjset")
+    async def fjset(self, ctx: commands.Context):
+        """Funnyjunk settings."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @fjset.command(name="fjsession")
+    async def set_fjsession(self, ctx: commands.Context, fjsession: str):
+        """Set the fjsession cookie."""
+        await self.config.fjsession.set(fjsession)
+        await ctx.reply("fjsession set.")
+        self.session.cookies.set("fjsession", fjsession)
+
+    @commands.is_owner()
+    @commands.hybrid_command(name="setcookies")
+    async def set_cookies(self, ctx: commands.Context, userId: str, fjsession: str):
+        """Set the cookies needed to bypass login protection."""
+        await self.config.userId.set(userId)
+        await self.config.fjsession.set(fjsession)
+        await ctx.reply("Cookies set.")
+
     @commands.hybrid_command(name="fj")
     async def convert(self, ctx: commands.Context, link: str):
         """Extract the raw video content from a funnyjunk link."""
@@ -21,7 +60,7 @@ class IspyFJ(commands.Cog):
             return await ctx.reply("That's not a funnyjunk link.", ephemeral=True)
         try:
             # make the request with the fake user agent
-            response = requests.get(link, headers={"User-Agent": "Mozilla/5.0"})
+            response = self.session.get(link, headers={"User-Agent": "Mozilla/5.0"})
             response.raise_for_status()
         except requests.HTTPError:
             return await ctx.reply("Failed to fetch the page.", ephemeral=True)
@@ -51,6 +90,7 @@ class IspyFJ(commands.Cog):
         finally:
             # close the file if it's open
             video_file.close()
+
 
 class VideoNotFoundError(Exception):
     pass
