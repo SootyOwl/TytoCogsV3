0b15d12e7f254d1e3ff2469633dca9d550314e9b
Sooty <7614538+SootyOwl@users.noreply.github.com>
2025-07-30 07:03:55 +0000

    fix(tldw): specify type for bot parameter in setup function

--- tldw/__init__.py
+++ tldw/__init__.py
@@ -1,7 +1,8 @@
 from tldw.tldw import TLDWatch
+from redbot.core.bot import Red
 
 __red_end_user_data_statement__ = "This cog does not store end user data."
 
 
-async def setup(bot):
+async def setup(bot: Red):
     await bot.add_cog(TLDWatch(bot))
